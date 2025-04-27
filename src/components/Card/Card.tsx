35d6278e21edc9a97f1febf900b24d040ef11220
UeberTimei <super.timati04@gmail.com>
2025-04-27 20:31:50 +0300

    feat: finished Cards page

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Card.module.scss";
 
-const Card = () => {
-  return <div>Card</div>;
+interface CardProps {
+  elevation?: 0 | 1 | 2 | 3;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Card: React.FC<CardProps> = ({
+  elevation = 0,
+  children,
+  className,
+}) => {
+  const cardClasses = classNames(
+    styles.card,
+    {
+      [styles["card--elevation-0"]]: elevation === 0,
+      [styles["card--elevation-1"]]: elevation === 1,
+      [styles["card--elevation-2"]]: elevation === 2,
+      [styles["card--elevation-3"]]: elevation === 3,
+    },
+    className
+  );
+
+  return <div className={cardClasses}>{children}</div>;
 };
-
-export default Card;
--- src/pages/CardsPage/CardsPage.tsx
+++ src/pages/CardsPage/CardsPage.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Card } from "../../components/Card/Card";
+import styles from "./CardsPage.module.scss";
 
-const CardsPage = () => {
-  return <div>CardsPage</div>;
+export const CardsPage: React.FC = () => {
+  return (
+    <div className={styles.cardsPage}>
+      <div className={styles.cardsPage__container}>
+        <div className={styles.cardsPage__item}>
+          <Card elevation={0}>
+            <p className={styles.cardsPage__item__title}>Card / Elevation 0</p>
+            <p className={styles.cardsPage__item__text}>Flat Card</p>
+          </Card>
+        </div>
+        <div className={styles.cardsPage__item}>
+          <Card elevation={1}>
+            <p className={styles.cardsPage__item__title}>Card / Elevation 1</p>
+            <p className={styles.cardsPage__item__text}>Floating Card</p>
+          </Card>
+        </div>
+        <div className={styles.cardsPage__item}>
+          <Card elevation={2}>
+            <p className={styles.cardsPage__item__title}>Card / Elevation 2</p>
+            <p className={styles.cardsPage__item__text}>
+              Inline dialogs and dropdowns
+            </p>
+          </Card>
+        </div>
+        <div className={styles.cardsPage__item}>
+          <Card elevation={3}>
+            <p className={styles.cardsPage__item__title}>Card / Elevation 3</p>
+            <p className={styles.cardsPage__item__text}>Dialogs</p>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardsPage;
