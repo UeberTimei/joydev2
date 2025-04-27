957ce7e144299384fe52fada3928a3987ddfbe4a
UeberTimei <super.timati04@gmail.com>
2025-04-27 21:11:06 +0300

    feat: finished Badges page

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Badge.module.scss";
 
-const Badge = () => {
-  return <div>Badge</div>;
+interface BadgeProps {
+  variant?: "subtle" | "solid";
+  color?:
+    | "neutral"
+    | "green"
+    | "blue"
+    | "red"
+    | "orange"
+    | "purple"
+    | "yellow"
+    | "teal";
+  value: number | string;
+  className?: string;
+  isPill?: boolean;
+}
+
+export const Badge: React.FC<BadgeProps> = ({
+  variant = "subtle",
+  color = "neutral",
+  isPill = false,
+  value,
+  className,
+}) => {
+  const displayValue = typeof value === "number" && value > 99 ? "99+" : value;
+
+  return (
+    <span
+      className={classNames(
+        styles.badge,
+        styles[variant],
+        styles[color],
+        isPill && styles.pill,
+        className
+      )}
+    >
+      {displayValue}
+    </span>
+  );
 };
-
-export default Badge;
