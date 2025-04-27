c687a1ca3c811e513aabb348d841513d1ab73512
UeberTimei <super.timati04@gmail.com>
2025-04-27 18:15:40 +0300

    feat: finished Headings page

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Heading.module.scss";
 
-const Headings = () => {
-  return <div>Headings</div>;
+interface HeadingProps {
+  level?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+  children: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const Heading: React.FC<HeadingProps> = ({
+  level = 500,
+  children,
+  className,
+  style,
+}) => {
+  const headingClass = classNames(
+    styles.heading,
+    {
+      [styles[`level${level}`]]: true,
+    },
+    className
+  );
+
+  return (
+    <h1 className={headingClass} style={style}>
+      {children}
+    </h1>
+  );
 };
 
-export default Headings;
+export default Heading;
