d93350a4e288ae7e1a9d0fe5eef63e5ca40e513d
UeberTimei <super.timati04@gmail.com>
2025-04-27 18:51:56 +0300

    feat: finished Paragraphs page

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import cn from "classnames";
+import styles from "./Paragraph.module.scss";
 
-const Paragraphs = () => {
-  return <div>Paragraphs</div>;
+interface ParagraphProps {
+  size: "300" | "400" | "500";
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Paragraph: React.FC<ParagraphProps> = ({
+  size,
+  children,
+  className,
+}) => {
+  return (
+    <p
+      className={cn(
+        styles.paragraph,
+        {
+          [styles[`paragraph--size-${size}`]]: size,
+        },
+        className
+      )}
+    >
+      {children}
+    </p>
+  );
 };
-
-export default Paragraphs;
--- src/pages/ParagraphsPage/ParagraphsPage.tsx
+++ src/pages/ParagraphsPage/ParagraphsPage.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Paragraph } from "../../components/Paragraph/Paragraph";
+import styles from "./ParagraphsPage.module.scss";
 
-const ParagraphsPage = () => {
-  return <div>ParagraphsPage</div>;
+export const ParagraphsPage: React.FC = () => {
+  const sampleText =
+    "Used for multiline pieces of content. Lorem ipsum dolor sit amet, ex lucilius hendrerit vim, tempor scaevola iudicabit ei ius, te eum illud impetus antiopam. Eu wisi commune voluptat pro, usu at alii magna aperiam.";
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h2 className={styles.title}>Paragraph / 300</h2>
+        <Paragraph size="300">{sampleText}</Paragraph>
+      </div>
+
+      <div>
+        <h2 className={styles.title}>Paragraph / 400</h2>
+        <Paragraph size="400">{sampleText}</Paragraph>
+      </div>
+
+      <div>
+        <h2 className={styles.title}>Paragraph / 500</h2>
+        <Paragraph size="500">{sampleText}</Paragraph>
+      </div>
+    </div>
+  );
 };
 
 export default ParagraphsPage;
