c3aa5bb518b49c4fe1ef4ab2a1227b3a586749d6
UeberTimei <super.timati04@gmail.com>
2025-04-27 21:39:47 +0300

    feat: finished Toast page

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import cn from "classnames";
+import styles from "./Toast.module.scss";
+import Text from "../Text/Text";
 
-const Toast = () => {
-  return <div>Toast</div>;
+interface ToastProps {
+  variant: "success" | "info" | "warning" | "error";
+  message: string;
+  description?: string;
+  className?: string;
+}
+
+export const Toast: React.FC<ToastProps> = ({
+  variant,
+  message,
+  description,
+  className,
+}) => {
+  const getIcon = () => {
+    switch (variant) {
+      case "success":
+        return "✔";
+      case "info":
+        return "ℹ";
+      case "warning":
+        return "⚠";
+      case "error":
+        return "❗";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        styles.toast,
+        {
+          [styles.success]: variant === "success",
+          [styles.info]: variant === "info",
+          [styles.warning]: variant === "warning",
+          [styles.error]: variant === "error",
+          [styles.hasDescription]: !!description,
+        },
+        className
+      )}
+    >
+      <span className={styles.icon}>{getIcon()}</span>
+      <div className={styles.content}>
+        <Text weight="semibold" className={styles.content__message} size={400}>
+          {message}
+        </Text>
+        {description && (
+          <Text
+            weight="default"
+            size={500}
+            className={styles.content__description}
+          >
+            {description}
+          </Text>
+        )}
+      </div>
+      <button className={styles.closeButton}>×</button>
+    </div>
+  );
 };
 
 export default Toast;
--- src/pages/ToastPage/ToastPage.tsx
+++ src/pages/ToastPage/ToastPage.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Toast } from "../../components/Toast/Toast";
+import styles from "./ToastPage.module.scss";
 
-const ToastPage = () => {
-  return <div>ToastPage</div>;
+export const ToastPage: React.FC = () => {
+  const loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.toasts}>
+        <Toast
+          variant="success"
+          message="YOU HAVE SUCCESSFULLY ENABLED PERSONAS."
+        />
+        <Toast
+          variant="info"
+          message="THERE ARE OVER 180 INTEGRATIONS AVAILABLE."
+        />
+        <Toast
+          variant="warning"
+          message="YOUR PLAN ONLY SUPPORTS A COMPUTE HISTORY OF 30 DAYS."
+        />
+        <Toast
+          variant="error"
+          message="WE WEREN’T ABLE TO SAVE YOUR CHANGES."
+        />
+        <Toast
+          variant="success"
+          message="YOU HAVE SUCCESSFULLY ENABLED PERSONAS."
+          description={loremIpsum}
+        />
+        <Toast
+          variant="info"
+          message="THERE ARE OVER 180 INTEGRATIONS AVAILABLE."
+          description={loremIpsum}
+        />
+        <Toast
+          variant="warning"
+          message="YOUR PLAN ONLY SUPPORTS A COMPUTE HISTORY OF 30 DAYS."
+          description={loremIpsum}
+        />
+        <Toast
+          variant="error"
+          message="WE WEREN’T ABLE TO SAVE YOUR CHANGES."
+          description={loremIpsum}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ToastPage;
