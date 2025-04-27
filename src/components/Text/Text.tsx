1d431f98bd7f15e4be26e0ff94d00d3c96473ba9
UeberTimei <super.timati04@gmail.com>
2025-04-27 18:33:17 +0300

    feat: finished Text page

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import classNames from "classnames";
+import styles from "./Text.module.scss";
 
-const Text = () => {
-  return <div>Text</div>;
+interface TextProps {
+  size?: 300 | 400 | 500;
+  weight?: "default" | "medium" | "semibold";
+  isLink?: boolean;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Text: React.FC<TextProps> = ({
+  size = 400,
+  weight = "default",
+  isLink = false,
+  children,
+  className,
+}) => {
+  const textClass = classNames(
+    styles.text,
+    {
+      [styles[`text--size-${size}`]]: size,
+      [styles[`text--weight-${weight}`]]: weight,
+      [styles["text--link"]]: isLink,
+    },
+    className
+  );
+
+  return <span className={textClass}>{children}</span>;
 };
 
 export default Text;
